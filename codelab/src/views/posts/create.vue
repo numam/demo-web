37f9ee697fa9f33526944ba50e8c712dcac16be5
yusuf n <yusufnurm@gmail.com>
2024-12-18 17:01:49 +0700

    modul 6

---
+++ codelab/src/views/posts/create.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        HALAMAN POSTS CREATE
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
